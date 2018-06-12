9679df19c912c5d467d0d7a36ec39a246f948704
yangs <yangs@gisuni.com>
2018-06-12 15:11:02 +0800

    Serving the HTML(about path) and create a index.html file(with some simple CSS style)

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-    res.send('<h1> Hello World </h1>');
+    // res.send('<h1> Hello World </h1>');
+    res.sendFile(__dirname + '/src/index.html');
 });
 
 http.listen(3000, function() {
